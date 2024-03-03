3c3dc0f2d939095a304b1eb9f798bc7737a805ae
M Anand Krishna <40604290+ANANDKRISHNAM@users.noreply.github.com>
2024-03-03 15:23:50 +0530

    Code Uploaded

---
+++ Code/GGL/constants.py
@@ -0,0 +1,12 @@
+"""
+Constants
+"""
+
+# ---- data -----
+
+celeba_path = '/Users/manandkrishna/Documents/s2/VisualComputing/GGL'
+imagenet_path = ''
+
+# ---- model ----
+
+celeba_gan_path = '/Users/manandkrishna/Documents/s2/VisualComputing/GGL/untitled folder 2'
